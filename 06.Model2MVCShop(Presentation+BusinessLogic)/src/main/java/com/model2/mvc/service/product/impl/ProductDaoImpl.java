afd4b9a0fbf72150a715ebbe4b46be39115d05c4
junseok <junseok@gmail.com>
2018-11-07 16:23:38 +0900

    1차수정완료

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/service/product/impl/ProductDaoImpl.java
@@ -0,0 +1,63 @@
+package com.model2.mvc.service.product.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductDao;
+
+
+
+@Repository("productDaoImpl")
+public class ProductDaoImpl implements ProductDao {
+	
+	///Field
+	///SqlSession阑 DAO后俊 流立 林涝.
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	
+	///Constructor
+	public ProductDaoImpl() {
+		System.out.println(this.getClass());
+	}
+	
+	public void addProduct(Product product) throws Exception {
+		sqlSession.insert("ProductMapper.addProduct", product);
+		
+	}
+	
+	public Product getProudct(int prodNo) throws Exception {
+		
+	
+		return sqlSession.selectOne("ProductMapper.getProduct", prodNo);
+		
+	}
+	
+	
+	
+	public void updateProduct(Product product) throws Exception {
+		sqlSession.update("ProductMapper.updateProduct", product);
+	}
+	
+
+	public List<Product> getProductList(Search search) throws Exception {
+		return sqlSession.selectList("ProductMapper.getProductList", search);
+	}
+
+
+	public int getTotalCount(Search search) throws Exception {
+		
+		return sqlSession.selectOne("ProductMapper.getTotalCount", search);
+	}
+	
+}
